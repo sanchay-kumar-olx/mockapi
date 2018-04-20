774d8a3ca8c3d1d5dcd706923dfb3658d9e74044
Sanchay <sanchay.kumar@olx.com>
2018-04-20 12:09:45 +0530

    mock api

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const koaMulter = require('koa-multer');
 
+const upload = koaMulter();
 const router = Router();
 
 router
-    .post('/api/v1/login', async function(ctx, next) {
-        ctx.body = { 
-		    "data": { 
-		        "token" : "alksjdflkajskdfljaslkdf" 
-		    } 
-		};
+    .post('/api/v1/login', upload.single(), async function(ctx, next) {
+    	const { email, password } = ctx.req.body;
+
+    	if (email === 'user@test.com' && password === 'qwerty') {
+			ctx.body = { 
+			    "data": { 
+			        "token" : "alksjdflkajskdfljaslkdf" 
+			    } 
+			};
+    	}
+        else {
+        	ctx.status = 400;
+
+        	const validation = [];
+
+        	if (email !== 'user@test.com') {
+        		validation.push({
+	                "field": "email",
+	                "message": "Please enter email in correct format"
+	            });
+        	}
+        	else {
+        		validation.push({
+	                "field": "password",
+	                "message": "Password length cannot be less than 3"
+	            });
+        	}
+
+        	ctx.body = {
+        		"error": {
+			        "status": 400,
+			        "title": "loginPayload",
+			        "validation": validation
+			    }
+			};
+        }
 
         await next();
     })
